e93385b7685d3abae58a855b65a74b2be385c0a6
pradyumna17 <psakolkar17@gmail.com>
2025-03-21 17:15:44 +0100

    Updated file for linter check

--- 1/solution.py
+++ 1/solution.py
@@ -1,16 +1,20 @@
-"""
-https://projecteuler.net/problem=1
-<p>If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, we get $3, 5, 6$ and $9$. The sum of these multiples is $23$.</p>
-<p>Find the sum of all the multiples of $3$ or $5$ below $1000$.</p>
+#!/usr/bin/env python3
 
-<p>Solution: 233168<p>
+"""
+Description: https://projecteuler.net/problem=1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Solution: 233168
 """
 
-def sumOfDivisors(n,target):
+
+def sumOfDivisors(n, target):
     x = target // n
-    return int(n * x * (x+1) / 2)
+    return int(n * x * (x + 1) / 2)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     target = 999
     sol = sumOfDivisors(3, target) + sumOfDivisors(5, target) - sumOfDivisors(15, target)
     print(sol)
